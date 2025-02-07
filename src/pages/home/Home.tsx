c541f3ab0a5984069591a024ddb6446f10a72aa8
ullas-kiran <ullas261@gmail.com>
2025-02-07 11:42:37 +0530

    feat(slider):custom slider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,47 +1,56 @@
-import BannerImg from "@/assets/images/hexnode-kiosk.webp";
-import Achievements from "./achievements/Achievements";
+import Achievements from "@/pages/home/achievements/Achievements";
+import HeroSection from "@/pages/home/banner/HeroSection";
+
 const Home: React.FunctionComponent = (): JSX.Element => {
   return (
     <>
-      <section className="p-[90px_0_30px] xl:p-[120px_0_0px] bg-[#020A19]">
-        <div className="w-[88%] max-w-[1300px] !m-auto">
-          <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-between">
-            <div className=" flex flex-col max-w-full sm:max-w-[550px] xl:max-w-[498px] order-2 xl:order-1">
-              <div>
-                <div className="mb-[20px] md:mb-[40px]">
-                  <h1 className="text-[#FFFFFF] text-[34px] leading-[44px] text-center lg:text-[42px] lg:leading-[53px] xl:text-left font-bold antialiased">
-                    Turn your devices into kiosks in a few minutes with Hexnode
-                    UEM
-                  </h1>
-                </div>
-              </div>
-              <div className="banner_footer">
-                <form className="p-0 m-0">
-                  <div className="flex justify-between flex-col items-center sm:flex-row solutions_wrapper__6y5DU">
-                    <div className="w-full">
-                      <input type="text" />
-                    </div>
-                    <div className="w-full">
-                      <button className="text-[16px] leading-[20px] uppercase text-white font-normal bg-[#dd0735] w-full border-0 flex justify-center items-center transition-all ease-in-out cursor-pointer hover:text-white hover:bg-[#bb022a] hover:no-underline focus:text-white focus:bg-[#bb022a] focus:no-underline SignupStripe_btn-label__U_7uu">
-                        GET STARTED NOW!
-                      </button>
-                    </div>
+      <HeroSection />
+      <Achievements />
+      <div className="py-[60px] flex justify-center items-center text-center flex-col lg:py-[120px]">
+        <div className="mx-auto max-w-[1300px] w-[88%]">
+          <h2 className="text-center mx-auto text-[#020a19] text-[28px] leading-[1.25] antialiased font-[700] sm:text-[36px] max-w-[900px]">
+            Specific kiosk modes for unique use cases
+          </h2>
+          <div className="pt-[40px] sm:pt-[60px] ">
+            <div className="relative hidden sm:block ">
+              <div className="flex bg-white justify-center relative">
+                {/* child-head */}
+                <div className="w-full relative border border-solid border-[#f7f7f7] rounded-t-[4px] overflow-hidden overflow-x-auto scroll-smooth">
+                  <div className="w-full scroll-smooth overflow-x-scroll relative inline-block no-scrollbar">
+                    <div
+                      className="h-[80px] transition-all duration-[600ms] z-2 ease-[cubic-bezier(0.25, 0.1, 0.25, 1)] bg-[#020a19] rounded-[4px] opacity-1 absolute bottom-0"
+                      style={{ width: "260px", left: "0px" }}
+                    ></div>
+                    <ul className="flex duration-300 ease-in justify-between items-center scroll-smooth whitespace-nowrap cursor-pointer">
+                      {[
+                        "Single App Kiosk",
+                        "Multi-App Kiosk",
+                        "Web-based Kiosk",
+                        "Digital Signages",
+                        "ASAM Kiosk",
+                      ].map((tab, index) => (
+                        <li
+                          key={index}
+                          className="flex-1 min-h-[80px] w-full px-[30px] relative text-center whitespace-normal transition-all duration-[400ms] ease-[cubic-bezier(0.25, 0.1, 0.25, 1)] before:content-[''] before:absolute before:left-0 before:w-[2px] before:h-full before:z-1 before:bg-[#f7f7f7]"
+                        >
+                          <p
+                            className={`p-[26px_0] max-w-[240px] h-[80px] w-full flex justify-center items-center text-[22px] whitespace-nowrap font-[600] leading-[24px] relative z-3 duration-[400ms] ease-[cubic-bezier(0.25, 0.1, 0.25, 1)] ${
+                              index === 0 ? "text-[#fff]" : "text-[#020a19]/50"
+                            }`}
+                          >
+                            <span>{tab}</span>
+                          </p>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </form>
-                <div>
-                  <span>error</span>
                 </div>
-              </div>
-            </div>
-            <div className="w-full max-w-full md:max-w-[597px] order-1 xl:order-2">
-              <div className="w-full max-w-full md:max-w-[597px]  max-[420px]:max-h-[258px] max-[420px]:mb-[40px]">
-                <img src={BannerImg} alt="banner" />
+                {/* child-head */}
               </div>
             </div>
           </div>
         </div>
-      </section>
-      <Achievements/>
+      </div>
     </>
   );
 };
