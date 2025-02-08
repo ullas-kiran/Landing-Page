255f22db33f1cb8a0f2e5a645baf9a3fcd971acc
ullas-kiran <ullas261@gmail.com>
2025-02-08 13:26:57 +0530

    feat(slider):custom slider

---
+++ src/assets/svg/RightArrow.tsx
@@ -0,0 +1,8 @@
+
+const RightArrow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="6.575" height="11.737" className="ml-[5px] translate-x-0 group-hover/tabLink:translate-x-[3px] transition-all duration-300 ease-out"><path fill="none" stroke="#dd0735" stroke-linecap="round" d="m.707.707 5.161 5.161-5.161 5.161" data-name="arrow - icon"></path></svg>
+  )
+}
+
+export default RightArrow
