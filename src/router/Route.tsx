cb52765031e79d1af219589618859deda8f58596
ullas-kiran <ullas261@gmail.com>
2025-02-10 21:33:49 +0530

    fix(router):router path changes

--- src/router/Route.tsx
+++ src/router/Route.tsx
@@ -2,25 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 // components
-import MainLayout from "@/layout/MainLayout";
+const MainLayout=lazy(()=>import("@/layout/MainLayout"));
 import RouterError from "@/components/errorBoundary/RouterError";
 
 // pages
 const Home = lazy(() => import("@/pages/home/Home"));
 
+const LazyWrapper: React.FunctionComponent<{Component: React.ComponentType<any>; fallback?: React.ReactNode}> = ({ Component }) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Component />
+  </Suspense>
+);
+
 const Router: React.FunctionComponent = (): JSX.Element => {
   const routes = [
     {
       path: "/",
-      element: <MainLayout />,
+      element: <LazyWrapper Component={MainLayout} />,
       children: [
         {
-         path:"",
-          element: (
-            <Suspense fallback={<div>loading........</div>}>
-              <Home />
-            </Suspense>
-          ),
+          path: "",
+          element: (<Home />),
           ErrorBoundary: RouterError,
         },
       ],
