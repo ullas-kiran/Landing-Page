91b6948772c1fcad69e60a7772eed39526b624ca
ullas-kiran <ullas261@gmail.com>
2025-02-11 00:03:14 +0530

    fix(svg):svg camelcase fix

--- src/assets/svg/Tick.tsx
+++ src/assets/svg/Tick.tsx
@@ -2,7 +2,7 @@
 
 const Tick = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14.393" height="10.111"><path fill="none" stroke="#60c458" stroke-width="2" d="M.697 4.107 5.45 8.718l8.247-8" data-name="tick - icon"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="14.393" height="10.111"><path fill="none" stroke="#60c458" strokeWidth="2" d="M.697 4.107 5.45 8.718l8.247-8" data-name="tick - icon"></path></svg>
   )
 }
 
